cf5d1db56efce648ea0a1f160694a8b45cbe70e2
emily <emily_tan_jia_en@students.edu.sg>
2026-07-04 14:22:30 +0800

    Initial Commit

---
+++ screensApp/screensAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  screensAppApp.swift
+//  screensApp
+//
+//  Created by T Krobot on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct screensAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
